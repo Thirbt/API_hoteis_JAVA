6c2900e3289610443ee3e8d1d16dc0b2e581d9f0
Thirbt <thiago.godo@hotmail.com>
2024-09-06 22:42:18 -0300

    Adicionando o usuarioDTO dentro do contrato de service da entidade usuario

--- src/main/java/com/hoteis/hoteis/service/contracts/UsuarioServiceContract.java
+++ src/main/java/com/hoteis/hoteis/service/contracts/UsuarioServiceContract.java
@@ -1,12 +1,13 @@
 package com.hoteis.hoteis.service.contracts;
 import java.util.List;
 
+import com.hoteis.hoteis.dto.UsuarioDTO;
 import com.hoteis.hoteis.models.Usuario;
 
 public interface UsuarioServiceContract {
     List<Usuario> obterTodosusuarios();
     Usuario obterUsuarioPorId(Long id);
-    Usuario inserirUsuario(Usuario usuario);
-    Usuario editarUsuario(Long id, Usuario usuario);
+    Usuario inserirUsuario(UsuarioDTO usuarioDTO);
+    Usuario editarUsuario(Long id, UsuarioDTO usuarioDTO);
     void deletarUsuario(Long id);
 }
